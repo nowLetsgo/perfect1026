9bc9ff6bb04e6a07e643d58d4fb08063d095307b
nowLetsgo <lipeihua0922@163.com>
2020-12-21 11:49:40 +0800

    day10：上午

---
+++ day11-JS高级/06.h5多线程/worker.js
@@ -0,0 +1,20 @@
+//自身接受，不需要使用任何对象绑定事件
+onmessage = function (mes) {
+    //参数就是MessageEvent 保存的是postMessage发送的内容对象
+    //参数对象的data属性就是发生的内容
+    console.log(mes);
+    console.log(mes.data);
+
+    //开启分线程计算
+    var num = 0;
+    for (var i = 0; i < mes.data; i++) {
+        num += Math.sqrt(i);
+    }
+
+    //计算完成num之后 要发送给主线程
+    postMessage(num)
+
+
+    // 在分线程上关闭线程：
+    close()
+}
